a87ff14a90b90ec37baaceaf4bb59ac3f5e475d2
yoshoku <yoshoku@outlook.com>
2022-01-05 00:25:05 +0900

    refactor!: remove existing native extension codes

--- ext/numo/libsvm/kernel_type.h
+++
@@ -1,9 +0,0 @@
-#ifndef NUMO_LIBSVM_KERNEL_TYPE_H
-#define NUMO_LIBSVM_KERNEL_TYPE_H 1
-
-#include <svm.h>
-#include <ruby.h>
-
-void rb_init_kernel_type_module();
-
-#endif /* NUMO_LIBSVM_KERNEL_TYPE_H */
--- ext/numo/libsvm/svm_type.h
+++
@@ -1,9 +0,0 @@
-#ifndef NUMO_LIBSVM_SVM_TYPE_H
-#define NUMO_LIBSVM_SVM_TYPE_H 1
-
-#include <svm.h>
-#include <ruby.h>
-
-void rb_init_svm_type_module();
-
-#endif /* NUMO_LIBSVM_SVM_TYPE_H */
