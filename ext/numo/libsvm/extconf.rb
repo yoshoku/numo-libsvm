45171d07d8c56de481fc825b1189f31f3e09ecdc
yoshoku <yoshoku@outlook.com>
2022-11-27 21:38:26 +0900

    fix: add dynamic_lookup linker option for macOS and Ruby 3.1

--- ext/numo/libsvm/extconf.rb
+++ ext/numo/libsvm/extconf.rb
@@ -20,6 +20,12 @@ if RUBY_PLATFORM =~ /mswin|cygwin|mingw/
   abort 'libnarray.a not found.' unless have_library('narray', 'nary_new')
 end
 
+if RUBY_PLATFORM.match?(/darwin/) && Gem::Version.new('3.1.0') <= Gem::Version.new(RUBY_VERSION)
+  if try_link('int main(void){return 0;}', '-Wl,-undefined,dynamic_lookup')
+    $LDFLAGS << ' -Wl,-undefined,dynamic_lookup'
+  end
+end
+
 abort 'libstdc++ is not found.' unless have_library('stdc++')
 
 $srcs = Dir.glob("#{$srcdir}/**/*.cpp").map { |path| File.basename(path) }
