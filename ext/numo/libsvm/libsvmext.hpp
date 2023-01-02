1fa0e2c1fff0944f13e484e5e33c73987847adbd
yoshoku <yoshoku@outlook.com>
2023-01-02 09:07:30 +0900

    docs: docs: update copyright year

--- ext/numo/libsvm/libsvmext.hpp
+++ ext/numo/libsvm/libsvmext.hpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2019-2022 Atsushi Tatsuma
+ * Copyright (c) 2019-2023 Atsushi Tatsuma
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
