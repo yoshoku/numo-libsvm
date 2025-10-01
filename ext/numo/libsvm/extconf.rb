03bfdd7700da974048a7ef121fe9622e6c342a17
yoshoku <yoshoku@outlook.com>
2025-10-01 21:04:53 +0900

    chore: look for libc++ on macOS

--- ext/numo/libsvm/extconf.rb
+++ ext/numo/libsvm/extconf.rb
@@ -26,7 +26,18 @@ if RUBY_PLATFORM.match?(/darwin/) && Gem::Version.new('3.1.0') <= Gem::Version.n
   end
 end
 
-abort 'libstdc++ is not found.' unless have_library('stdc++')
+have_libcpp = false
+if RUBY_PLATFORM.include?('darwin')
+  if have_library('c++')
+    have_libcpp = true
+  else
+    warn 'libc++ is not found.'
+  end
+end
+
+if !have_libcpp && !RUBY_PLATFORM.include?('mswin')
+  warn 'libstdc++ is not found.' unless have_library('stdc++')
+end
 
 $srcs = Dir.glob("#{$srcdir}/**/*.cpp").map { |path| File.basename(path) }
 $INCFLAGS << " -I$(srcdir)/src"
